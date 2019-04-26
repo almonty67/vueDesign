c5b83d4f9e0ced5336c5133d9a5413b5f241677f
almonty <17941439+almonty67@users.noreply.github.com>
2019-04-26 16:45:40 -0500

    changed line 3 and 4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import Argon from "./plugins/argon-kit";
+import router from "./startRouter";
+import Argon from "@/plugins/argon-kit";
 import './registerServiceWorker'
 
 Vue.config.productionTip = false;
